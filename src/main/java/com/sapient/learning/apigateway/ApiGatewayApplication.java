23b9d0a823601d9f3ca2357c82d6ad8271948c88
Kumar Chandrakant <kchandrakant@gmail.com>
2019-06-07 13:17:31 +0530

    Modified zuul properties.

--- src/main/java/com/sapient/learning/apigateway/ApiGatewayApplication.java
+++ src/main/java/com/sapient/learning/apigateway/ApiGatewayApplication.java
@@ -1,8 +1,12 @@
 package com.sapient.learning.apigateway;
 
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
 
+
+@EnableZuulProxy
 @SpringBootApplication
 public class ApiGatewayApplication {
 
